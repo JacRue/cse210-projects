9cfd8e1fe405546838c1f691db7cba32bd7a0b42
jacrue <ruesch0311@gmail.com>
2023-02-08 23:46:10 -0600

    Add more scriptures to SMG and a new class

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,13 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Create a new instance of the Scripture class with a reference to John 3:16
-        Scripture scripture = new Scripture("John 3:16", "For God so loved the world that he gave his one and only Son, that whoever believes in him shall not perish but have eternal life.");
+        // Create a new instance of the ScriptureCollection class
+        ScriptureCollection scriptureCollection = new ScriptureCollection();
 
-        // Play the scripture memory game
-        scripture.Play();
+        // Add multiple scriptures to the collection
+        scriptureCollection.AddScripture(new Scripture("John 3:16", "For God so loved the world that he gave his one and only Son, that whoever believes in him shall not perish but have eternal life."));
+        scriptureCollection.AddScripture(new Scripture("Proverbs 3:5-6", "Trust in the Lord with all your heart and lean not on your own understanding; In all your ways submit to him, and he will make your paths straight."));
+        scriptureCollection.AddScripture(new Scripture("1 Nephi 1:8", "And being thus overcome with the Spirit, he was carried away in a vision, even that he saw the heavens open, and he thought he saw God sitting upon his throne, surrounded with numberless concourses of angels in the attitude of singing and praising their God."));
+        scriptureCollection.AddScripture(new Scripture("1 Nephi 3:19", "And behold, it is wisdom in God that we should obtain these records, that we may preserve unto our children the language of our fathers;"));
+
+        // Play all the scriptures in the collection
+        scriptureCollection.PlayScriptures();
     }
 }
+
+class ScriptureCollection
+{
+    public List<Scripture> Scriptures { get; set; }
+
+    public ScriptureCollection()
+    {
+        Scriptures = new List<Scripture>();
+    }
+
+    public void AddScripture(Scripture scripture)
+    {
+        Scriptures.Add(scripture);
+    }
+
+    public void PlayScriptures()
+    {
+        Console.WriteLine("Playing all scriptures in the collection:");
+        foreach (Scripture scripture in Scriptures)
+        {
+            scripture.Play();
+        }
+    }
+}
+
+
 
 class Scripture
 {
@@ -50,6 +82,7 @@ class Scripture
             }
 
             Console.WriteLine("\n");
+            Console.WriteLine("Scripture Memory Game: The game will cycle through each set of scriptures until all scriptures have played");
             Console.WriteLine("Press enter to hide another word, or type quit to exit");
 
             // Read the user input
